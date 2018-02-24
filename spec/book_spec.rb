c3e1f03722ba0191339b142f9ef69ae3adcd2214
Natalie <njseeto@users.noreply.github.com>
2018-02-24 16:44:32 +0000

    3 more failing tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -16,4 +16,19 @@ describe Book do
             lambda {Book.new "Title", "Author"}.should raise_exception ArgumentError
         end
     end
+    describe "#title" do
+        it "returns the correct title" do
+            @book.title.should eql "Title"
+        end
+    end 
+    describe "#author" do
+        it "returns the correct author" do
+            @book.author.should eql "Author"
+        end
+    end 
+    describe "#category" do
+        it "returns the correct category" do
+            @book.category.should eql :category
+        end
+    end 
 end
